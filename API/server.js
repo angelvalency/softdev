edf3f06798a5a11caa302c17dd43aaa2da0e0f61
angelvalency <angelvalency05@gmail.com>
2024-04-25 21:39:18 +0700

    update server.js

--- API/server.js
+++ API/server.js
@@ -1,16 +1,20 @@
-var express = require('express');
-var app = express();
-var fs = require('fs');
+const express = require('express');
+const app = express();
+const fs = require('fs');
 
 app.get('/getUsers', function(req, res){
-    fs.readFile(__dirname + "/" + "users.json", 'utf8', function(err, data){
+    fs.readFile(__dirname + "/users.json", 'utf8', function(err, data){
+        if (err) {
+            console.error("Error reading file:", err);
+            return res.status(500).send("Internal Server Error");
+        }
         console.log(data);
         res.end(data);
     });
-})
+});
 
-var server = app.listen(8080, function(){
-    var host = server.address().address
-    var port = server.address().port
-    console.log("REST API demo app listening at http://%s:%s", host, port)
-})
+const server = app.listen(8080, function(){
+    //const host = server.address().address;
+    const port = server.address().port;
+    console.log("REST API demo app listening at http://localhost:%s", port);
+});
